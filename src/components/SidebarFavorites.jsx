17ec9ffd588553695590d76bffc6d646d8e5c7b9
naastichaak <nk01022008@gmail.com>
2024-03-14 19:47:49 +0200

    edded favourites sidebar

--- src/components/SidebarFavorites.jsx
+++ src/components/SidebarFavorites.jsx
@@ -1,4 +1,173 @@
+import { Heading, Stack, Text, Flex, Button } from '@chakra-ui/react';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+
 function SidebarFavorites() {
-  return <div>SidebarFavorites</div>;
+  return (
+    <Stack>
+      <Stack
+        padding="15px 10px"
+        borderRadius="5px"
+        backgroundColor="#0008"
+        rowGap="20px"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Stack
+          width="100%"
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading color="white" fontSize="30px" fontWeight="600">
+            My Favorites
+          </Heading>
+
+          <Text color="#D6D10F">See All</Text>
+        </Stack>
+
+        <Flex
+          flexDirection="column"
+          gap="10px"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Flex gap="10px" alignItems="center" justifyContent="center">
+            <Stack
+              width="70px"
+              height="70px"
+              borderRadius="5px"
+              backgroundColor="pink"
+            ></Stack>
+
+            <Flex flexDirection="column" alignItems="flex-start">
+              <Text color="white" fontWeight="600">
+                Dead Eyes
+              </Text>
+              <Text color="white" fontSize="14px" fontWeight="400">
+                Promoting Sounds...
+              </Text>
+            </Flex>
+
+            <Button variant="ghost">
+              <BsThreeDotsVertical fontSize="25px" color="white" />
+            </Button>
+          </Flex>
+
+          <Flex gap="10px" alignItems="center" justifyContent="center">
+            <Stack
+              width="70px"
+              height="70px"
+              borderRadius="5px"
+              backgroundColor="pink"
+            ></Stack>
+
+            <Flex flexDirection="column" alignItems="flex-start">
+              <Text color="white" fontWeight="600">
+                Wish I Was Better
+              </Text>
+              <Text color="white" fontSize="14px" fontWeight="400">
+                Promoting Sounds...
+              </Text>
+            </Flex>
+
+            <Button variant="ghost">
+              <BsThreeDotsVertical fontSize="25px" color="white" />
+            </Button>
+          </Flex>
+
+          <Flex gap="10px" alignItems="center" justifyContent="center">
+            <Stack
+              width="70px"
+              height="70px"
+              borderRadius="5px"
+              backgroundColor="pink"
+            ></Stack>
+
+            <Flex flexDirection="column" alignItems="flex-start">
+              <Text color="white" fontWeight="600">
+                Dead Eyes
+              </Text>
+              <Text color="white" fontSize="14px" fontWeight="400">
+                Promoting Sounds...
+              </Text>
+            </Flex>
+
+            <Button variant="ghost">
+              <BsThreeDotsVertical fontSize="25px" color="white" />
+            </Button>
+          </Flex>
+
+          <Flex gap="10px" alignItems="center" justifyContent="center">
+            <Stack
+              width="70px"
+              height="70px"
+              borderRadius="5px"
+              backgroundColor="pink"
+            ></Stack>
+
+            <Flex flexDirection="column" alignItems="flex-start">
+              <Text color="white" fontWeight="600">
+                Dead Eyes
+              </Text>
+              <Text color="white" fontSize="14px" fontWeight="400">
+                Promoting Sounds...
+              </Text>
+            </Flex>
+
+            <Button variant="ghost">
+              <BsThreeDotsVertical fontSize="25px" color="white" />
+            </Button>
+          </Flex>
+
+          <Flex gap="10px" alignItems="center" justifyContent="center">
+            <Stack
+              width="70px"
+              height="70px"
+              borderRadius="5px"
+              backgroundColor="pink"
+            ></Stack>
+
+            <Flex flexDirection="column" alignItems="flex-start">
+              <Text color="white" fontWeight="600">
+                Dead Eyes
+              </Text>
+              <Text color="white" fontSize="14px" fontWeight="400">
+                Promoting Sounds...
+              </Text>
+            </Flex>
+
+            <Button variant="ghost">
+              <BsThreeDotsVertical fontSize="25px" color="white" />
+            </Button>
+          </Flex>
+
+          <Flex gap="10px" alignItems="center" justifyContent="center">
+            <Stack
+              width="70px"
+              height="70px"
+              borderRadius="5px"
+              backgroundColor="pink"
+            ></Stack>
+
+            <Flex flexDirection="column" alignItems="flex-start">
+              <Text color="white" fontWeight="600">
+                Dead Eyes
+              </Text>
+              <Text color="white" fontSize="14px" fontWeight="400">
+                Promoting Sounds...
+              </Text>
+            </Flex>
+
+            <Button variant="ghost">
+              <BsThreeDotsVertical fontSize="25px" color="white" />
+            </Button>
+          </Flex>
+        </Flex>
+      </Stack>
+    </Stack>
+  );
 }
 export default SidebarFavorites;
