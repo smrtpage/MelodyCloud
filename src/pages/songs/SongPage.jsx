a5cda29e0dfadf30853471951bd75a7e9e1c7451
naastichaak <nk01022008@gmail.com>
2024-03-28 12:44:20 +0200

    added song page

--- src/pages/songs/SongPage.jsx
+++ src/pages/songs/SongPage.jsx
@@ -1,10 +1,18 @@
 import { Heading, Stack, Text, Flex, Button } from '@chakra-ui/react';
+import Navbar from '../../components/Navbar';
+// import Navigation from '../../components/Navigation';
+import { FaRegHeart } from 'react-icons/fa';
+import { BsThreeDots } from 'react-icons/bs';
+import { FaPlay } from 'react-icons/fa';
 
 function SongPage() {
   return (
     <>
+      <Navbar />
+
       <Stack
-        padding="15px 10px"
+        width="100%"
+        padding="50px 40px"
         borderRadius="5px"
         rowGap="20px"
         display="flex"
@@ -12,16 +20,51 @@ function SongPage() {
         // alignItems="center"
         // justifyContent="center"
       >
-        <Stack>
+        <Flex gap="20px" alignItems="flex-end">
           <Stack
-            width="130px"
-            height="130px"
+            width="200px"
+            height="200px"
             borderRadius="5px"
             backgroundColor="pink"
           ></Stack>
-        </Stack>
+
+          <Flex flexDirection="column">
+            <Heading fontSize="60px" fontWeight="500">
+              Trance
+            </Heading>
+            <Flex gap="10px" alignItems="center">
+              <Text fontSize="30px" fontWeight="400">
+                Metro Boomin
+              </Text>
+              <Text fontSize="30px" fontWeight="400">
+                2.345.000
+              </Text>
+              <Text fontSize="30px" fontWeight="400">
+                December, 20, 2022
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
       </Stack>
+
+      <Flex gap="20px" alignItems="center" padding="0 40px">
+        <Button
+          width="70px"
+          height="70px"
+          borderRadius="50%"
+          backgroundColor="#D6D10F"
+        >
+          <FaPlay fontSize="25px" />
+        </Button>
+        <Button variant="ghost">
+          <FaRegHeart fontSize="25px" />
+        </Button>
+        <Button variant="ghost">
+          <BsThreeDots fontSize="25px" />
+        </Button>
+      </Flex>
     </>
   );
 }
+
 export default SongPage;
